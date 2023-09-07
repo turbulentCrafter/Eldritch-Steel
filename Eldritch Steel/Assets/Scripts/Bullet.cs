d8c7ae4f267f5da0191a2917e2f4598659a3b730
Auguste2435 <80259028+Auguste2435@users.noreply.github.com>
2023-09-07 16:48:30 +0200

    Šautuvas šauna
    Step 2.

---
+++ Eldritch Steel/Assets/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float life = 3;
+
+
+    void Awake()
+    {
+        Destroy(gameObject, life);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        Destroy(collision.gameObject);
+        Destroy(gameObject);
+    }
+}
